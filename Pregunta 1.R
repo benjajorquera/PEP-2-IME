9df2c7ff0d20afb6e768968b25ad1fe9200ee90e
benjajorquera <benja.jorquera.j@gmail.com>
2022-01-21 19:31:37 -0300

    primeras condiciones p1

--- Pregunta 1.R
+++ Pregunta 1.R
@@ -18,17 +18,48 @@
 
 # Como se trata de una muestra con más de dos medias y cada evaluación es
 # realizada sobre un mismo Lavatrooper, se utilizará la prueba omni-bus ANOVA
-# de una vía para muestras correlacionadas, para responder a la pregunta
-# anterior, y posteriormente realizar un análisis post-hoc de los resultados.
+# de una vía para muestras correlacionadas con un nivel de confianza de 95% para
+# responder a la pregunta anterior, y posteriormente realizar un análisis
+# post-hoc de los resultados.
 
 # Para esto primero se cargan los datos del archivo csv:
-datos <- read.csv2(file.choose(), header=TRUE)
+datos_lavatroopers <- read.csv2(file.choose(), header=TRUE)
 
 # Se filtran los datos seleccionando a los reclutas "Lavatroopers" y las
 # evaluaciones de sus oficiales:
-datos <- datos %>% filter(division == "Lavatrooper") %>% select(eval_instructor, 
-                                                                eval_capitan, 
-                                                                eval_comandante, 
-                                                                eval_general)
+datos_lavatroopers <- datos_lavatroopers %>%filter(division == 
+                                                     "Lavatrooper") %>% 
+  select(eval_instructor, eval_capitan, eval_comandante, eval_general)
 
+# La pregunta detrás de esta prueba es ¿se diferencias las medias poblacionales?
+# lo cual nos permite plantear las siguientes hipótesis de acuerdo a lo
+# solicitado:
+
+# H0: la calificación promedio de la evaluación realizada por cada uno de los
+# oficiales a los Lavatroopers es la misma.
+# H1: la calificación promedio de la evaluación realizada por cada uno de los
+# oficiales a los Lavatroopers es diferente para al menos un oficial.
+
+# 1. La escala con que se mide la variable dependiente tiene las propiedades de
+# una escala de intervalos iguales.
+# Esto se cumple ya que los puntajes asignados a la calificación operan bajo
+# esta condición.
+
+# 2. Las mediciones son independientes al interior de cada grupo. 
+# Se cumple ya que cada observación representa un oficial diferente, por lo
+# tanto son independientes.
+
+# 3. Se puede suponer razonablemente que la(s) población(es) de origen sigue(n)
+# una distribución normal.
+
+# Para verificar esto se utilizará la prueba de Shapiro de Normalidad, además se
+# manipularan los datos para utilizar verificar la cuarta condición y utilizar
+# el procedimiento ANOVA posteriormente:
+
+#El dataframe es convertido a formato largo
+datos_lavatroopers <- datos_lavatroopers %>% pivot_longer(c("eval_instructor",
+                                                            "eval_capitan",
+                                                            "eval_comandante",
+                                                            "eval_general"),
+                                  names_to = "oficiales", values_to = "puntaje")
 
